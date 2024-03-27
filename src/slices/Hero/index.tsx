28735e36b10efc0b3ab48748570598b23dfac8df
8703 Studio <72730252+8703studio@users.noreply.github.com>
2024-03-27 03:07:25 +0100

    add dependencies
    Dependencies added

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,27 @@
+import { Content } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+import { SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <>{slice.primary.studioname}</>
+      <>{slice.primary.tagline}</>
+      <PrismicNextImage field={slice.primary.heroimg} />
+
+    </section>
+  );
+};
+
+export default Hero;
